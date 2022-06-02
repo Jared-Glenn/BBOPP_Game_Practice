23b8cf0d5bfd8a012ff867be96ff2c570a95a1cf
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-06-01 20:51:15 -0600

    Blackjack v2

--- 004_Blackjack.py
+++ 004_Blackjack.py
@@ -1,27 +1,27 @@
-"""Blackjack, by Al Sweigart"""
+"""Blackjack, by Al Sweigart
+The classic card game also known as 21. (This version doesn't have splitting or insurance.)
 
 
 
-
-
+"""
 
 import random, sys
 
-#Set up the constants:
-HEARTS = chr(9829)
+# Set up the constants:
+HEARTS = chr(9829) # Character 9829 is heart.
 DIAMONDS = chr(9830)
 SPADES = chr(9824)
 CLUBS = chr(9827)
-
+# (A list of chr codes is at https://inventwith python.com/charactermap)
 BACKSIDE = 'backside'
 
 
 def main():
     print('''Blackjack, by Al Sweigart
-
+          
     Rules:
         Try to get as close to 21 without going over.
-        Kings, Queens, and Jacks are worth 10 points.
+        Kings, Queens, and Jacks are worth their face value.
         Aces are worth 1 or 11 points.
         Cards 2 through 10 are worth their face value.
         (H)it to take another card.
@@ -29,19 +29,84 @@ def main():
         On your first play, you can (D)ouble down to increase your bet
         but must hit exactly one more time before standing.
         In case of a tie, the bet is returned to the player.
-        The dealer stop hitting at 17.''')
-
+        The dealer stops hitting at 17.''')
+    
     money = 5000
-    while True:
-        #Check if the player has run out of money:
+    while True: # Main game loop.
+        # Check if the player has frun out of money:
         if money <= 0:
             print("You're broke!")
             print("Good thing you weren't playing with real money.")
-            print('Thanks for playing!')
+            print("Thanks for playing!")
             sys.exit()
-
-        #Let the player enter their bet for this round:
-        print('Money:' , money)
+            
+        # Let the player enter their bet for this round:
+        print('Money:', money)
         bet = getBet(money)
-
-        #Give the dealer and player two cards from the deck each:
+        
+        # Give the dealer and player two cards from the deck each:
+        deck = getDeck()
+        dealerHand = [deck.pop(), deck.pop()]
+        playerHand = [deck.pop(), deck.pop()]
+        
+        #Handle player actions:
+        print('Bet:', bet)
+        while True: # Keep looping until player stands or busts.
+            displayHands(playerHand, dealerHand, False)
+            print()
+            
+            # Check if the player has bust:
+            if getHandValue(playerHand) > 21:
+                break
+            
+            # Get the players' move, either H, S, or D:
+            move = getMove(playerHand, money - bet)
+            
+            # Handle the player actions:
+            if move == 'D':
+                # Player is doubling down, they can increase their bet:
+                additionalBet = getBet(min(bet, (money - bet)))
+                bet += additionalBet
+                print(f'Bet increased to {bet}')
+                print('Bet:', bet)
+                
+                if move in ('H', 'D'):
+                    # Hit/doubleing down takes another card.
+                    newCard = deck.pop()
+                    rank, suit = newCard
+                    print(f'You drew a {rank} of {suit}.')
+                    playerHand.append(newCard)
+                    
+                    if getHandValue(playerHand) > 21:
+                        # The player has busted:
+                        continue
+                
+                if move in ('S', 'D'):
+                    # Stand/doubling down stops the player's turn.
+                    break
+                
+                # Handle the dealer's actions:
+                if getHandValue(playerHand) <= 21:
+                    while getHandValue(dealerHand) < 17:
+                        # The dealer hits:
+                        print('Dealer hits...')
+                        dealerHand.append(deck.pop())
+                        displayHands(playerHand, dealerHand, False)
+                        
+                        if getHandValue(dealerHand) > 21:
+                            break # The dealer has busted.
+                        input('Press Enter to continue...')
+                        print('\n\n')
+                            
+                # Show the final hands:
+                displayHands(playerHand, dealerHand, True)
+                
+                playerValue = getHandValue(playerHand)
+                dealerValue = getHandValue(dealerHand)
+                # Handle whether the player won, lost, or tied:
+                if dealerValue > 21:
+                    print(f'Dealer busts! You win ${bet}!')
+                    money += bet
+                elif (playerValue > 21) or (playerValue < dealerValue):
+                    print('You lost!')
+                    
